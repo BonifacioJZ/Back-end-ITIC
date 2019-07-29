7806528623545935e4d766ce45c20c592fc792a5
Bonifacio Juarez Ceja <revanjz6@gmail.com>
2019-07-29 14:33:25 -0500

    Ultimos Toques

--- iticbackend/iticbackend/settings.py
+++ iticbackend/iticbackend/settings.py
@@ -27,8 +27,7 @@ DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = [
-    'http://localhost:8080',
-    'localhost'
+    
 ]
 
 
@@ -57,7 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 
 ]
 
--- iticbackend/iticbackend/wsgi.py
+++ iticbackend/iticbackend/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+#sys.path.append('/home/revan/Desarrollo/Back-end-ITIC/iticbackend')
+#os.environ['DJANGO_SETTING_MODULE']="iticbackend.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iticbackend.settings')
 
 application = get_wsgi_application()
